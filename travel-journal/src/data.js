9d02168cbfc31433057a613e09cade8fbcb79e2a
NeeshuSharmaa <neeshusharmaa@gmail.com>
2022-11-01 03:21:31 +0530

    Updated: images & URL

--- travel-journal/src/data.js
+++ travel-journal/src/data.js
@@ -6,7 +6,7 @@ export default [
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "Distinctive music, Creole cuisine, unique dialects, and its annual celebrations and festivals, most notably Mardi Gras. The historic heart of the city is the French Quarter, known for its French and Spanish Creole architecture and vibrant nightlife along Bourbon Street.",
-        imageUrl: "https://unsplash.com/photos/0838a5VoJ2Y"
+        imageUrl: "https://images.unsplash.com/photo-1568693059993-a239b9cd4957?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
     }, 
     {
         title: "Paris",
@@ -15,7 +15,8 @@ export default [
         startDate: "25 May, 2021",
         endDate: "10 June, 2021",
         description: "City of lights, city of love, the fashion capital,all with bustling river banks, the beauty and charm of its architecture, the romantic atmosphere, the delicious food, countless opportunities to explore art, culture, fashion and history",
-        imageUrl: "https://unsplash.com/photos/ZG5_505mUog"
+        // imageUrl: "https://unsplash.com/photos/ZG5_505mUog" URL ERROR: this url doesn't load the image so, i apparently copied image address & then pasted here
+        imageUrl: "https://images.unsplash.com/photo-1495442358998-961b69f45703?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
     }, 
     {
         title: "Maldives",
@@ -24,6 +25,6 @@ export default [
         startDate: "14 Sep, 2021",
         endDate: "22 Sep, 2021",
         description: ' “the tropical paradise", the ultimate dream of luxury and tranquility, quite irresistible with its white beaches, turquoise sea, blue lagoons, colourful marine life and swaying palm trees.',
-        imageUrl: "https://unsplash.com/photos/M1ObxvsWVhY"
+        imageUrl: "https://images.unsplash.com/photo-1602217401979-6f7209f5289d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fG1hbGRpdmVzJTIwcmVzb3J0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
     }
     ]
