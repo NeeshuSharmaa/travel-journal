5028d539a6be9d0677eaa1121da3414a0e1ce803
NeeshuSharmaa <neeshusharmaa@gmail.com>
2022-11-01 02:30:27 +0530

    Created Navbar

---
+++ travel-journal/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar() {
+    return(
+        <div className="navbar">
+            <img className="nav-img" src="images/world.svg" alt=""></img>
+            <span className="nav-txt">My Travel Journal</span>
+        </div>
+    )
+}
