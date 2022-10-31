24ca05f719f04d1c5df7ca2c382dd02b913b588b
NeeshuSharmaa <neeshusharmaa@gmail.com>
2022-11-01 02:13:48 +0530

    Created data.js

---
+++ travel-journal/src/data.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        title: "New Orleans",
+        location: "USA",
+        googleMapsUrl: "https://goo.gl/maps/sXhqu7kwEMovbL5u9",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Distinctive music, Creole cuisine, unique dialects, and its annual celebrations and festivals, most notably Mardi Gras. The historic heart of the city is the French Quarter, known for its French and Spanish Creole architecture and vibrant nightlife along Bourbon Street.",
+        imageUrl: "https://unsplash.com/photos/0838a5VoJ2Y"
+    }, 
+    {
+        title: "Paris",
+        location: "France",
+        googleMapsUrl: "https://goo.gl/maps/bYRreNxZsizuqL5dA",
+        startDate: "25 May, 2021",
+        endDate: "10 June, 2021",
+        description: "City of lights, city of love, the fashion capital,all with bustling river banks, the beauty and charm of its architecture, the romantic atmosphere, the delicious food, countless opportunities to explore art, culture, fashion and history",
+        imageUrl: "https://unsplash.com/photos/ZG5_505mUog"
+    }, 
+    {
+        title: "Maldives",
+        location: "Indiana Ocean",
+        googleMapsUrl:"https://goo.gl/maps/7SvVrASADuXNc6NS8" ,
+        startDate: "14 Sep, 2021",
+        endDate: "22 Sep, 2021",
+        description: ' “the tropical paradise", the ultimate dream of luxury and tranquility, quite irresistible with its white beaches, turquoise sea, blue lagoons, colourful marine life and swaying palm trees.',
+        imageUrl: "https://unsplash.com/photos/M1ObxvsWVhY"
+    }
+    ]
