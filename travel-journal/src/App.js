ce3968066a3eae76fb274b04dd6122256d1680eb
NeeshuSharmaa <neeshusharmaa@gmail.com>
2022-11-01 03:04:49 +0530

    Renderd: with props

--- travel-journal/src/App.js
+++ travel-journal/src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar"
+import Traveldetails from "./components/Traveldetails"
+import data from "./data.js";
 
 export default function App() {
+  var placeDetails=data.map((item)=>{
+    return <Traveldetails item={item}/>;
+  });
+
   return (
     <div className="App">
       <Navbar/>
+      {placeDetails}
       
     </div>
   );
